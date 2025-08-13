407e4944e448dde02a2a2742cc86916439984ab4
Chaitanya varma <varmac301@gmail.com>
2025-08-13 21:46:25 +0530

    Delete agents directory

--- agents/src/agents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AI Agents package for the AIOps Platform
-"""
-from .terraform_agent import TerraformAgent
-from .kubernetes_agent import KubernetesAgent
-
-__all__ = ['TerraformAgent', 'KubernetesAgent']
